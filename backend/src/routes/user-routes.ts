98bb27eabc403399460bb7ed9c70df94e60c94cf
Kaua Aldrovandi <kaua.aldrovandi1@gmail.com>
2024-09-24 12:40:59 -0300

    signup validation

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,10 @@
-import { Router } from 'express';
-import { getAllUsers } from '../controllers/user-controllers.js';
+import { Router } from 'express'
+import { getAllUsers, userSignup } from '../controllers/user-controllers.js'
+import { signupValidator, validate } from '../utils/validators.js'
 
 const userRoutes = Router()
 
-userRoutes.get("/", getAllUsers)
+userRoutes.get('/', getAllUsers)
+userRoutes.post('/signup', validate(signupValidator), userSignup)
 
 export default userRoutes
