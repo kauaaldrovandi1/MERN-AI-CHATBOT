d9c54b3e938ca437609ea6247265454e3ba7afa4
Kaua Aldrovandi <kaua.aldrovandi1@gmail.com>
2024-09-25 13:31:51 -0300

    auth_token

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,16 +2,18 @@ import express from 'express'
 import { config } from 'dotenv'
 import morgan from 'morgan'
 import appRouter from './routes/index.js'
+import cookieParser from 'cookie-parser'
 config()
 
 const app = express()
 
 // middlewares
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 // remove it in prod
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 
-app.use("/api/v1", appRouter)
+app.use('/api/v1', appRouter)
 
 export default app
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 
 import User from '../models/User.js'
+import { createToken } from '../utils/token-manager.js'
+import { COOKIE_NAME } from '../utils/constants.js'
 
 export const getAllUsers = async (
   req: Request,
@@ -26,9 +28,73 @@ export const userSignup = async (
   try {
     // user signup
     const { name, email, password } = req.body
+    const existingUser = await User.findOne({ email })
+    if (existingUser) return res.status(401).send('User already register')
     const hashedPassword = await hash(password, 10)
     const user = new User({ name, email, password: hashedPassword })
     await user.save()
+
+    // create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      domain: 'localhost',
+      httpOnly: true,
+      signed: true,
+    })
+
+    const token = createToken(user._id.toString(), user.email, '7d')
+    const expires = new Date()
+    expires.setDate(expires.getDate() + 7)
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires,
+      httpOnly: true,
+      signed: true,
+    })
+
+    return res.status(200).json({ message: 'OK', id: user._id.toString() })
+  } catch (error) {
+    console.log(error)
+    return res.status(200).json({ message: 'ERROR', cause: error.message })
+  }
+}
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    // user login
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(401).send('User not registered')
+    }
+    const isPasswordCorrect = await compare(password, user.password)
+    if (!isPasswordCorrect) {
+      return res.status(403).json('Incorrect password')
+    }
+
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      domain: 'localhost',
+      httpOnly: true,
+      signed: true,
+    })
+
+    const token = createToken(user._id.toString(), user.email, '7d')
+    const expires = new Date()
+    expires.setDate(expires.getDate() + 7)
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires,
+      httpOnly: true,
+      signed: true,
+    })
+
     return res.status(200).json({ message: 'OK', id: user._id.toString() })
   } catch (error) {
     console.log(error)
